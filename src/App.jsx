dd82262b881480cdd44a6a65f05c6e61ca296a9c
Deepak-172003 <deepakkanojiya770@gmail.com>
2025-03-22 14:55:50 +0530

    normal project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+import Quiz from './Components/Quiz'
+
+function App() {
+  
+
+  return (
+    <>
+
+    <Quiz/>
+      
+    </>
+  )
+}
+
+export default App
